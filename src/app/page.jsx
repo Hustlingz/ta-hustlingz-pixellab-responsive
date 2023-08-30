f9994b1aeaad886a9eb89b39da4052fe773bfeab
Alin Voicu <alin.voicu.25@gmail.com>
2023-08-30 14:24:03 +0200

    Thread Affair Update! 5

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,18 +1,55 @@
-import Header from '@/components/common/Header';
+import ProductTile from '@/components/catalog/ProductTile';
 import Footer from '@/components/common/Footer';
+import Header from '@/components/common/Header';
+import HomeCatalogPreview from '@/components/home/HomeCatalogPreview';
+import HomeNewsletter from '@/components/home/HomeNewsletter';
+import Payment from '@/components/home/Payment';
 
 export default function Home() {
   return (
-    <>
+    <div className="home">
       <header className="header container mt-lg-4">
         <Header></Header>
       </header>
 
-      <main className="content"></main>
+      <main className="content">
+        <div className="layout-css">
+          <HomeCatalogPreview></HomeCatalogPreview>
+        </div>
+
+        <div className="layout-flex">
+          <HomeCatalogPreview></HomeCatalogPreview>
+        </div>
+
+        <div className="layout-grid">
+          <HomeCatalogPreview></HomeCatalogPreview>
+        </div>
+
+        {/* layout bootstrap style */}
+        <div className="container">
+          <ul className="row justify-content-between">
+            <li className="col-12 col-lg-3 mb-5">
+              <ProductTile></ProductTile>
+            </li>
+
+            <li className="col-12 col-lg-3 mb-5">
+              <ProductTile></ProductTile>
+            </li>
+
+            <li className="col-12 col-lg-3">
+              <ProductTile></ProductTile>
+            </li>
+          </ul>
+        </div>
+
+        <HomeNewsletter></HomeNewsletter>
+
+        <Payment></Payment>
+      </main>
 
       <footer className="footer">
         <Footer></Footer>
       </footer>
-    </>
+    </div>
   );
 }
